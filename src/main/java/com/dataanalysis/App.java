49184ca6450f45d32e5ee450493b4f3617a20457
JacobTurner1801 <jacobmturner@gmail.com>
2025-02-25 16:36:08 +0000

    create my own csv reader class to load the csv, gets the header, format, and all records in the csv file

--- src/main/java/com/dataanalysis/App.java
+++ src/main/java/com/dataanalysis/App.java
@@ -1,57 +1,26 @@
 package com.dataanalysis;
 
-import java.io.FileReader;
+import java.util.List;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+
+import com.dataanalysis.csv.CSVReader;
 
 public class App {
     public static void main( String[] args ) throws Exception {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(i + " " + args[i]);
-        }
+        // for (int i = 0; i < args.length; i++) {
+        //     System.out.println(i + " " + args[i]);
+        // }
         String filePath = args[0];
         String targetColumn = args[1];
 
-        FileReader reader = new FileReader(filePath);
-        CSVParser firstPassParser = new CSVParser(reader, CSVFormat.DEFAULT);
-        CSVRecord firstRecord = firstPassParser.iterator().next();
-        firstPassParser.close();
-        reader.close();
-
-        boolean hasHeader = false;
-        for (String val : firstRecord) {
-            try {
-                Double.parseDouble(val);
-            } catch (NumberFormatException e) {
-                hasHeader = true;
-                break;
-            }
+        CSVReader csvReader = new CSVReader(filePath);
+        try {
+            List<CSVRecord> records = csvReader.load();
+            System.out.println("total records: " + records.size());
+            System.out.println("loaded");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
-
-        reader = new FileReader(filePath);
-        CSVFormat format;
-        if (hasHeader) {
-            format = CSVFormat.DEFAULT.builder().setHeader(firstRecord.values()).build();
-        } else {
-            String[] header = generateDefaultHeader(firstRecord.size());
-            format = CSVFormat.DEFAULT.builder().setHeader(header).build();
-        }
-        CSVParser actualParser = new CSVParser(reader, format);
-
-        for (CSVRecord record : actualParser) {
-            System.out.println(record.get(targetColumn));
-        }
-        actualParser.close();
-        reader.close();
-    }
-
-    private static String[] generateDefaultHeader(int columns) {
-        String[] header = new String[columns];
-        for (int i = 0; i < columns; i++) {
-            header[i] = "Column" + (i+1);
-        }
-        return header;
     }
 }
---
+++ src/main/java/com/dataanalysis/csv/CSVReader.java
@@ -0,0 +1,90 @@
+package com.dataanalysis.csv;
+
+import java.io.FileReader;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class CSVReader {
+    private String filePath;
+    private CSVRecord firstRecord;
+
+    public CSVReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * Loads the csv file, including getting the header and the format
+     * @return list of records for the csv file
+     * @throws Exception
+     */
+    public List<CSVRecord> load() throws Exception {
+        FileReader reader = new FileReader(this.filePath);
+        boolean hasHeader = this.hasHeader(reader);
+        reader = new FileReader(filePath); // reset reader
+        CSVFormat format = this.createCSVFormat(hasHeader, this.firstRecord);
+        CSVParser parser = new CSVParser(reader, format);
+        List<CSVRecord> records = parser.getRecords();
+        parser.close();
+        reader.close();
+        return records;
+    }
+
+    /**
+     * Creates the column -> value map format based on the first record
+     * @param hasHeader - whether or not the first record defines the columns or not
+     * @param firstRecord - actual first record of the csv (might be a header)
+     * @return - loaded csv format
+     */
+    private CSVFormat createCSVFormat(boolean hasHeader, CSVRecord firstRecord) {
+        CSVFormat format;
+        if (hasHeader) {
+            format = CSVFormat.DEFAULT.builder().setHeader(firstRecord.values()).build();
+        } else {
+            System.err.println("You have not defined a header, so there is no target to go against, you should define a header");
+            String[] header = generateDefaultHeader(firstRecord.size());
+            format = CSVFormat.DEFAULT.builder().setHeader(header).build();
+        }
+        return format;
+    }
+
+    /**
+     * Whether or not a csv file has a header
+     * @param reader
+     * @return if csv file has header
+     * @throws Exception - can throw IOException or NoSuchElementException
+     * @implNote - closes the reader given to it, so you have to create it again afterwards
+     */
+    private boolean hasHeader(FileReader reader) throws Exception {
+        CSVParser firstPassParser = new CSVParser(reader, CSVFormat.DEFAULT);
+        this.firstRecord = getFirstRecord(firstPassParser);
+        boolean hasHeader = false;
+        // note: may be able to use firstRecord.isMapped() somehow
+        for (String val : firstRecord) {
+            try {
+                Double.parseDouble(val);
+            } catch (NumberFormatException e) {
+                hasHeader = true;
+                break;
+            }
+        }
+        firstPassParser.close();
+        reader.close();
+        return hasHeader;
+    }
+
+    private CSVRecord getFirstRecord(CSVParser parser) throws Exception {
+        CSVRecord firstRecord = parser.iterator().next();
+        return firstRecord;
+    }
+
+    private String[] generateDefaultHeader(int columns) {
+        String[] header = new String[columns];
+        for (int i = 0; i < columns; i++) {
+            header[i] = "Column" + (i+1);
+        }
+        return header;
+    }
+}
