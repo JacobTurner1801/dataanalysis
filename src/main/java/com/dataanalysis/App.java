5137f85986247f8129e6fb669dbc6bcd419d9d3e
JacobTurner1801 <jacobmturner@gmail.com>
2025-02-25 16:49:36 +0000

    nothing major, figuring out how to access each record and display it

--- src/main/java/com/dataanalysis/App.java
+++ src/main/java/com/dataanalysis/App.java
@@ -1,13 +1,16 @@
 package com.dataanalysis;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.csv.CSVRecord;
 
 import com.dataanalysis.csv.CSVReader;
 
 public class App {
-    public static void main( String[] args ) throws Exception {
+    public static void main( String[] args ) {
         // for (int i = 0; i < args.length; i++) {
         //     System.out.println(i + " " + args[i]);
         // }
@@ -17,6 +20,12 @@ public class App {
         CSVReader csvReader = new CSVReader(filePath);
         try {
             List<CSVRecord> records = csvReader.load();
+            Map<String, String> eg = records.get(1).toMap();
+            Iterator<Entry<String, String>> it = eg.entrySet().iterator();
+            while (it.hasNext()) {
+                Entry<String, String> e = it.next();
+                System.out.println(e.getKey() + ", " + e.getValue());
+            }
             System.out.println("total records: " + records.size());
             System.out.println("loaded");
         } catch (Exception e) {
